4a90d5988342717468b60207edb1dd27085cee64
Salar Khosravi <salar.khosravi@gmail.com>
2025-02-14 23:02:33 +0330

    fix sqlite3 commands

--- session_01(sqlite).py
+++ session_01(sqlite).py
@@ -26,34 +26,33 @@ cursor = connection.cursor()
 
 # create users table
 #  --------------------------------
-create_users_table_query = '''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        full_name TEXT NOT NULL,
-        email TEXT NOT NULL UNIQUE,
-        marriage_status BOOLEAN NOT NULL default FALSE,
-        password TEXT NOT NULL,
-        balance Numeric NOT NULL default 0
-    )
-'''
-
-cursor.execute(create_users_table_query)
+# create_users_table_query = '''
+#     CREATE TABLE IF NOT EXISTS users (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         full_name TEXT NOT NULL,
+#         email TEXT NOT NULL UNIQUE,
+#         marriage_status BOOLEAN NOT NULL default FALSE,
+#         password TEXT NOT NULL,
+#         balance Numeric NOT NULL default 0
+#     )
+# '''
+# cursor.execute(create_users_table_query)
 
 
 
 
 # create users table
 #  ----------------------------------------
-create_products_table_query = '''
-    CREATE TABLE IF NOT EXISTS products (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL,
-        price Numeric NOT NULL,
-        is_exist BOOLEAN NOT NULL default TRUE,
-        description TEXT NULL default ''
-    )
-'''
-cursor.execute(create_products_table_query)
+# create_products_table_query = '''
+#     CREATE TABLE IF NOT EXISTS products (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         name TEXT NOT NULL,
+#         price Numeric NOT NULL,
+#         is_exist BOOLEAN NOT NULL default TRUE,
+#         description TEXT NULL default ''
+#     )
+# '''
+# cursor.execute(create_products_table_query)
 
 
 
@@ -61,71 +60,129 @@ cursor.execute(create_products_table_query)
 
 # Create Invoices table
 #  ----------------------------------------
-create_invoices_table_query = '''
-    CREATE TABLE IF NOT EXISTS invoices (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        user_id INTEGER,
-        product_id INTEGER,
-        quantity INTEGER NOT NULL,
-        total REAL NOT NULL,
-        FOREIGN KEY(user_id) REFERENCES users(id),
-        FOREIGN KEY(product_id) REFERENCES products(id)
-    )
-'''
-
-cursor.execute(create_invoices_table_query)
+# create_invoices_table_query = '''
+#     CREATE TABLE IF NOT EXISTS invoices (
+#         id INTEGER PRIMARY KEY AUTOINCREMENT,
+#         user_id INTEGER,
+#         product_id INTEGER,
+#         quantity INTEGER NOT NULL,
+#         total REAL NOT NULL,
+#         FOREIGN KEY(user_id) REFERENCES users(id),
+#         FOREIGN KEY(product_id) REFERENCES products(id)
+#     )
+# '''
+# cursor.execute(create_invoices_table_query)
 
 
 
 
 
 # ============ Create records (insert) ==============
-cursor = connection.cursor()
 
 # Insert into users
-cursor.execute("INSERT INTO users (full_name, email, marriage_status, password, balance) VALUES (?, ?, ?, ?, ?)", ("John Doe", "john@example.com", True, 'abc', 30))
+# cursor.execute("INSERT INTO users (full_name, email, marriage_status, password, balance) VALUES (?, ?, ?, ?, ?)", ("John Doe", "john@example.com", True, 'abc', 30))
+# connection.commit()
+# connection.close()
+
 
 # Insert into products
-cursor.execute("INSERT INTO products (name, price, is_exist, description) VALUES (?, ?, ?, ?)", ("Laptop", 999.99, True, 'empty'))
+# cursor.execute("INSERT INTO products (name, price, is_exist, description) VALUES (?, ?, ?, ?)", ("Laptop", 999.99, True, 'empty'))
+# connection.commit()
+# connection.close()
+
 
 # Insert into invoices (assuming user_id=1 and product_id=1 exist)
-cursor.execute("INSERT INTO invoices (user_id, product_id, quantity, total) VALUES (?, ?, ?, ?)",
-          (1, 1, 3, 103.55))
+# cursor.execute("INSERT INTO invoices (user_id, product_id, quantity, total) VALUES (?, ?, ?, ?)", (1, 1, 3, 120))
+# connection.commit()
+# connection.close()
 
 
 
 
+
+# Insert multiple value to user table
+# data = [
+#     ("Ali Akbari", 'ali@gmail.com', '123'),
+#     ("Sara Ahadi", 'sara@gmail.com', '321'),
+#     ("Ben Mira", 'ben@gmail.com', '333'),
+#     ("Saeed Sobhan", 'saeed@gmail.com', '4444'),
+#     ("Maryam Imani", 'maryam@gmail.com', '1111'),
+#
+# ]
+#
+# cursor.executemany("INSERT INTO users (full_name, email, password) VALUES(?, ?, ?)", data)
+# connection.commit()
+# connection.close()
+
+
+# Insert multiple value to products table
+# data = [
+#     ("Dell", 123),
+#     ("Iphone 16", 321),
+#     ("Samsung s20", 333),
+#     ("LG TV", 4444),
+#     ("Macbook", 1111),
+# ]
+#
+# cursor.executemany("INSERT INTO products (name, price) VALUES(?, ?)", data)
+# connection.commit()
+# connection.close()
+
+
+# Insert multiple value to invoice table
+# data = [
+#     (1, 2, 3, 110),
+#     (2, 3, 1, 120),
+#     (2, 4, 1, 300),
+#     (2, 5, 2, 200),
+#     (4, 1, 1, 150),
+#     (4, 3, 2, 120),
+#     (2, 4, 5, 270),
+# ]
+#
+# cursor.executemany("INSERT INTO invoices (user_id, product_id, quantity, total) VALUES(?, ?, ?, ?)", data)
+# connection.commit()
+# connection.close()
 
 
 
 # ============= Read sqlite =============
 
 # Select all users
-cursor.execute("SELECT * FROM users")
-users = cursor.fetchall()
-for user in users:
-    print(user)
+# cursor.execute("SELECT * FROM users")
+# users = cursor.fetchall()
+# for user in users:
+#     print(user)
+
+
 
 # Select specific user
-cursor.execute("SELECT * FROM users WHERE id = ?", (1,))
-user = cursor.fetchone()
-print(user)
+# cursor.execute("SELECT * FROM users WHERE id = 3")
+# user = cursor.fetchone()
+# print(user)
+
 
 
 # Join tables (e.g., invoices with users and products)
-cursor.execute('''
-        SELECT invoices.id, users.full_name, products.name, invoices.quantity, invoices.total 
-         FROM invoices
-         JOIN users ON invoices.user_id = users.id
-         JOIN products ON invoices.product_id = products.id
-     '''
-)
+# cursor.execute('''
+#         SELECT users.full_name, products.name, invoices.quantity, invoices.total
+#          FROM invoices
+#          JOIN users ON invoices.user_id = users.id
+#          JOIN products ON invoices.product_id = products.id
+#      '''
+# )
+#
+#
+# invoices = cursor.fetchall()
+# print('\n\ndata fetch from join')
+# print('-----------------------------------')
+# for invoice in invoices:
+#     print(invoice)
+#
+# print('\n\n\ndata fetch from join as a text')
+# print('-----------------------------------')
+# for invoice in invoices:
+#     print(f'{invoice[0]} has bought {invoice[2]} of {invoice[1]} for {invoice[3]} Euros')
 
-invoices = cursor.fetchall()
-for invoice in invoices:
-    print(invoice)
-
-
-connection.close()
 
 
